a498014aa8f414360c8cd5d795d26d0d5a58087a
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-09 15:31:58 -0700

    javascript flow and comments

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-// GIVEN a weather dashboard with form inputs
-
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
 //Variables for global scope
 var today = moment();
 var apiKey = "fa9deced30799df9282f5573c8325b98";
 var searchBar = document.querySelector("input");
 var city;
 
+//Set city through API call on lon/lat of input
 function getCity(city) {
     city = $("input").val()
 
@@ -45,6 +32,7 @@ function appendHistory(city) {
     $("#history").append(history);
 }
 
+//Generate forecasts for current city
 function fiveDay(lat, lon) {
     city = $("input").val()
 
@@ -163,9 +151,11 @@ function fiveDay(lat, lon) {
 
 }
 
+//Initiate forecast with search button
 $("#searchBtn").on("click", getCity)
 
 var title = $("<p>").text("5-day Weather Forecast")
 
+//Display current day and time
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 $("#currentDay").append(title)
