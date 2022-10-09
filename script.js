eb022971f1e7b7f1f660efd0bfa6ee0ac1775c46
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-09 14:05:36 -0700

    add city locator call

--- script.js
+++ script.js
@@ -11,15 +11,33 @@
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+//Variables for global scope
 var today = moment();
 var apiKey = "fa9deced30799df9282f5573c8325b98";
 var searchBar = document.querySelector("input");
 var city;
 
 function getCity(city){
-    city = ${"input"}.val()
+    city = $("input").val()
 
     var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}';
     appendHistory (city)
 
+    //Fetch info for city entered into locator.
+    fetch(queryURL)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            var lon = data.coord.lon
+            var lat = data.coord.lat
+            fiveDay(lat, lon)
+            
+            console.log(data)
+        })
+
 }
+
+$("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
+$("#currentDay").append(title)
